6887b87382214c48ecb7c5695d27f5b5ccafc8c8
david.m.cooke <devnull@localhost>
2007-12-24 14:08:58 +0000

    Rearrange so that all source is in numexpr/

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from numpy.distutils.misc_util import Configuration
 
 def configuration(parent_package='', top_path=None):
     config = Configuration('numexpr', parent_package, top_path)
-    config.add_extension('interpreter',
-                         sources = ['interpreter.c'],
-                         depends = ['interp_body.c',
-                                    'complex_functions.inc'],
+    config.add_extension('numexpr.interpreter',
+                         sources = ['numexpr/interpreter.c'],
+                         depends = ['numexpr/interp_body.c',
+                                    'numexpr/complex_functions.inc'],
                          extra_compile_args=['-O2', '-funroll-all-loops'],
                          )
-    config.add_data_dir('tests')
+    config.add_data_dir('numexpr/tests')
     return config
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
     setup(author='David M. Cooke',
           author_email='cookedm@physics.mcmaster.ca',
-          version='0.1',
+          version='0.8',
           zip_safe=False,
           **configuration(top_path='').todict())
