f36355a38718edae785b3fcf8f6e202a4cff2e74
david.m.cooke <devnull@localhost>
2008-03-12 15:59:31 +0000

    Move compiler.py to necompiler.py, to avoid conflict with the stdlib compiler module (which we may use some day).

--- numexpr/__init__.py
+++ numexpr/__init__.py
@@ -1,5 +1,5 @@
 from numexpr.info import __doc__
 from numexpr.expressions import E
-from numexpr.compiler import numexpr, disassemble, evaluate
+from numexpr.necompiler import numexpr, disassemble, evaluate
 
 __version__ = '1.0'
