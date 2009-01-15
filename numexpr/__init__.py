911dca761d47efec0f5ffdc4fbfb549a801b8449
Francesc Alted <faltet@gmail.com>
2009-01-15 17:58:46 +0000

    Yet another glitch with the VERSION file path discovery.

--- numexpr/__init__.py
+++ numexpr/__init__.py
@@ -1,6 +1,10 @@
+import os.path
+
 from numexpr.info import __doc__
 from numexpr.expressions import E
 from numexpr.necompiler import numexpr, disassemble, evaluate
 from numexpr.tests import test
 
-__version__ = open('VERSION').read().strip()
+dirname = os.path.dirname(__file__)
+
+__version__ = open(os.path.join(dirname,'VERSION')).read().strip()
