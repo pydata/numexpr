066729b9ab4e6f8984dcc16c35511523b50ecd13
Francesc Alted <faltet@gmail.com>
2009-02-09 18:55:20 +0000

    Added a minimalistic docstring for the package.  The idea is to replicate the docs in fewer places as possible.  As maximum the manual should be on the web site and the README file (and not sure about the latest).

--- numexpr/__init__.py
+++ numexpr/__init__.py
@@ -1,3 +1,16 @@
+"""
+Numexpr is a fast numerical expression evaluator for NumPy.  With it,
+expressions that operate on arrays (like "3*a+4*b") are accelerated
+and use less memory than doing the same calculation in Python.
+
+See:
+
+http://code.google.com/p/numexpr/
+
+for more info about it.
+
+"""
+
 from __config__ import show as show_config, get_info
 
 if get_info('mkl'):
@@ -6,7 +19,6 @@ else:
     use_vml = False
 
 import os.path
-from numexpr.info import __doc__
 from numexpr.expressions import E
 from numexpr.necompiler import numexpr, disassemble, evaluate
 from numexpr.tests import test
