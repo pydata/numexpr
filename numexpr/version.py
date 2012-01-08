776d98c0c5b8303209c052f25bc205f424ad6651
Francesc Alted <faltet@pytables.com>
2012-01-08 11:00:31 +0100

    Heating engines for 2.0.1 release.

--- numexpr/version.py
+++ numexpr/version.py
@@ -8,8 +8,8 @@
 #  rights to use.
 ####################################################################
 
-version='2.1'
-release=False
+version='2.0.1'
+release=True
 
 if not release:
     version += '.dev'
