7f87db1c699df07b7c0e8761c09e33e05193beea
Mark Wiebe <mwwiebe@gmail.com>
2012-11-13 14:00:46 -0800

    Change FindNumPy.cmake to work in the presence of nonnumerical markers like '1.7.0b2'
    Suggested by Daniel Müllner

--- FindNumPy.cmake
+++ FindNumPy.cmake
@@ -45,44 +45,46 @@ else()
     find_package(PythonInterp)
 endif()
 
-if(PYTHONINTERP_FOUND)
-    execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
-        "import numpy as n; print(n.__version__); print(n.get_include());"
-        RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
-        OUTPUT_VARIABLE _NUMPY_VALUES
-        ERROR_VARIABLE _NUMPY_ERROR_VALUE
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-    if(_NUMPY_SEARCH_SUCCESS MATCHES 0)
-        set(NUMPY_FOUND TRUE)
-
-        # Convert the process output into a list
-        string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES})
-        string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
-        list(GET _NUMPY_VALUES 0 NUMPY_VERSION)
-        list(GET _NUMPY_VALUES 1 NUMPY_INCLUDE_DIRS)
-
-        # Make sure all directory separators are '/'
-        string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
-
-        # Get the major and minor version numbers
-        string(REGEX REPLACE "\\.|rc|dev" ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
-        list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
-        list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
-        list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
-        math(EXPR NUMPY_VERSION_DECIMAL
-            "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
-
-        find_package_message(NUMPY
-            "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
-            "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
-    else()
-        if(NumPy_FIND_REQUIRED)
-            message(FATAL_ERROR
-                "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
-        endif()
-        set(NUMPY_FOUND FALSE)
-    endif()
-else()
+if(NOT PYTHONINTERP_FOUND)
     set(NUMPY_FOUND FALSE)
 endif()
+
+execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
+    "import numpy as n; print(n.__version__); print(n.get_include());"
+    RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
+    OUTPUT_VARIABLE _NUMPY_VALUES
+    ERROR_VARIABLE _NUMPY_ERROR_VALUE
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+if(NOT _NUMPY_SEARCH_SUCCESS MATCHES 0)
+    if(NumPy_FIND_REQUIRED)
+        message(FATAL_ERROR
+            "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
+    endif()
+    set(NUMPY_FOUND FALSE)
+endif()
+
+# Convert the process output into a list
+string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES})
+string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
+list(GET _NUMPY_VALUES 0 NUMPY_VERSION)
+list(GET _NUMPY_VALUES 1 NUMPY_INCLUDE_DIRS)
+
+# Make sure all directory separators are '/'
+string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
+
+# Get the major and minor version numbers
+string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
+list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
+list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
+list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
+string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
+math(EXPR NUMPY_VERSION_DECIMAL
+    "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
+
+find_package_message(NUMPY
+    "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
+    "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
+
+set(NUMPY_FOUND TRUE)
+
