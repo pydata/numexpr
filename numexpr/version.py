3b7092f5473134a23269e30d4b529e0526f515c1
Francesc Alted <faltet@gmail.com>
2013-04-14 17:12:34 +0200

    Releasing 2.1-rc1

--- numexpr/version.py
+++ numexpr/version.py
@@ -8,8 +8,8 @@
 #  rights to use.
 ####################################################################
 
-version='2.1b1'
-release=False
+version='2.1-rc1'
+release=True
 
 if not release:
     version += '.dev'
