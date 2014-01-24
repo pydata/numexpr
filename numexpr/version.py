9cb8478c440bd9f6bcbf98caedf306d722a1762b
Francesc Alted <francesc@continuum.io>
2014-01-24 13:30:49 +0100

    Releasing 2.3-rc1

--- numexpr/version.py
+++ numexpr/version.py
@@ -8,8 +8,8 @@
 #  rights to use.
 ####################################################################
 
-version='2.3'
-release=False
+version='2.3-rc1'
+release=True
 
 if not release:
     version += '.dev'
