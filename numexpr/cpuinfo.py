25afb5c615e130d61130b80c9d968c37c917b005
anatoly techtonik <techtonik@gmail.com>
2016-02-02 15:55:16 +0300

    cpuinfo.py: Bring back printing CPU info on command line

--- numexpr/cpuinfo.py
+++ numexpr/cpuinfo.py
@@ -796,16 +796,13 @@ if __name__ == "__main__":
     cpu.is_Intel()
     cpu.is_Alpha()
 
-    print
-    'CPU information:',
+    info = []
     for name in dir(cpuinfo):
         if name[0] == '_' and name[1] != '_':
             r = getattr(cpu, name[1:])()
             if r:
                 if r != 1:
-                    print
-                    '%s=%s' % (name[1:], r),
+                    info.append('%s=%s' % (name[1:], r))
                 else:
-                    print
-                    name[1:],
-    print
+                    info.append(name[1:])
+    print 'CPU information: ' + ' '.join(info)
