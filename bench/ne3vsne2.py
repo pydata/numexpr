df9479d06f6e40d70189cdcbea75b9dcd103b673
Robert A. McLeod <robbmcleod@gmail.com>
2017-02-14 12:20:55 +0100

    Latest benchmarks

--- bench/ne3vsne2.py
+++ bench/ne3vsne2.py
@@ -39,11 +39,11 @@ C_d = np.random.uniform( size=arrSize )
 out_d = np.zeros( arrSize )
 '''
 
-tries = 100
+tries = 50
 arraySizes = 2**np.arange(14,22)
-times_np = np.zeros_like(arraySizes, dtype='float64' )
-times_ne3 = np.zeros_like(arraySizes, dtype='float64' )
-times_ne2 = np.zeros_like(arraySizes, dtype='float64' )
+times_np = 1E6*np.ones_like(arraySizes, dtype='float64' )
+times_ne3 = 1E6*np.ones_like(arraySizes, dtype='float64' )
+times_ne2 = 1E6*np.ones_like(arraySizes, dtype='float64' )
 
 ne3.set_num_threads(8)
 ne2.set_num_threads(8)
@@ -68,10 +68,11 @@ for I, arrSize in enumerate( arraySizes ):
     B_d = np.random.uniform( size=arrSize )
     C_d = np.random.uniform( size=arrSize )
     out_d = np.zeros( arrSize )    
-    t0 = time()
+    
     for J in np.arange(tries):
-        ne3.evaluate( 'out_d=A_d*B_d + C_d', stackDepth=3 )
-    times_ne3[I] += time() - t0
+        t0 = time()
+        ne3.evaluate( 'out_d=A_d*B_d + C_d')
+        times_ne3[I] = np.minimum( time() - t0, times_ne3[I] )
 
 
 for I, arrSize in enumerate( arraySizes ):
@@ -80,18 +81,20 @@ for I, arrSize in enumerate( arraySizes ):
     B_d = np.random.uniform( size=arrSize )
     C_d = np.random.uniform( size=arrSize )
     out_d = np.zeros( arrSize )
-    t0 = time()
+
     for J in np.arange(tries):
+        t0 = time()
         ne2.evaluate( 'A_d*B_d + C_d', out=out_d )
+        times_ne2[I] = np.minimum( time() - t0, times_ne2[I] )
         # Turn off the CacheDict as it defeats the purpose of benchmarking here
         ne2._names_cache.clear()
         ne2._numexpr_cache.clear()
-    times_ne2[I] += time() - t0
+        
               
     
 times_np /= tries
-times_ne2 /= tries
-times_ne3 /= tries
+#times_ne2 /= tries
+#times_ne3 /= tries
 
 plt.figure()
 plt.plot( arraySizes/1024, times_np, '.-', label='NumPy', markerfacecolor='k' )
@@ -107,9 +110,9 @@ plt.savefig( "NE2vsNE3_float64.png", dpi=200 )
 
 
 ### COMPLEX64 ####
-times_np_F = np.zeros_like(arraySizes, dtype='float64' )
-times_ne3_F = np.zeros_like(arraySizes, dtype='float64' )
-times_ne2_F = np.zeros_like(arraySizes, dtype='float64' )
+times_np_F = 1E6*np.ones_like(arraySizes, dtype='float64' )
+times_ne3_F = 1E6*np.ones_like(arraySizes, dtype='float64' )
+times_ne2_F = 1E6*np.ones_like(arraySizes, dtype='float64' )
 
 
 cexpr = 'A_F*B_F + C_F'
@@ -135,10 +138,11 @@ for I, arrSize in enumerate( arraySizes ):
     B_F = np.random.uniform( size=arrSize ).astype( 'complex64' )
     C_F = np.random.uniform( size=arrSize ).astype( 'complex64' )
     out_F = np.zeros( arrSize, dtype='complex64' )    
-    t0 = time()
+    
     for J in np.arange(tries):
-        ne3.evaluate( 'out_F=A_F*B_F + C_F', stackDepth=3 )
-    times_ne3_F[I] += time() - t0
+        t0 = time()
+        ne3.evaluate( 'out_F=A_F*B_F + C_F' )
+        times_ne3_F[I] = np.minimum( time() - t0, times_ne3_F[I] )
 
 
 for I, arrSize in enumerate( arraySizes ):
@@ -147,18 +151,19 @@ for I, arrSize in enumerate( arraySizes ):
     B_F = np.random.uniform( size=arrSize ).astype( 'complex64' )
     C_F = np.random.uniform( size=arrSize ).astype( 'complex64' )
     out_D = np.zeros( arrSize, dtype='complex128' )
-    t0 = time()
     for J in np.arange(tries):
+        t0 = time()
         ne2.evaluate( 'A_F*B_F + C_F', out=out_D )
+        times_ne2_F[I] = np.minimum( time() - t0, times_ne2_F[I] )
         # Turn off the CacheDict as it defeats the purpose of benchmarking here
         ne2._names_cache.clear()
         ne2._numexpr_cache.clear()
-    times_ne2_F[I] += time() - t0
+        
               
-    
+# TODO: also make NumPy 'best-of' rather than a mean
 times_np_F /= tries
-times_ne2_F /= tries
-times_ne3_F /= tries
+#times_ne2_F /= tries
+#times_ne3_F /= tries
 
 plt.figure()
 plt.plot( arraySizes/1024, times_np_F, '.-', label='NumPy', markerfacecolor='k' )
@@ -171,3 +176,5 @@ plt.xlim( [0, np.max(arraySizes/1024)] )
 plt.ylim( [0, np.max(times_np)] )
 plt.title( "'a*b + c' with complex-64" )
 plt.savefig( "NE2vsNE3_complex64.png", dpi=200 )
+
+
