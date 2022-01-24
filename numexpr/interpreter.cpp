1c6a024947c3aa1bf926ecb9828036b306d7c6d7
Robert McLeod <robbmcleod@gmail.com>
2022-01-24 10:54:34 -0800

    Fix for #397, use of NPY_ARRAY_UPDATEIFCOPY flag

--- numexpr/interpreter.cpp
+++ numexpr/interpreter.cpp
@@ -1269,7 +1269,7 @@ NumExpr_run(NumExprObject *self, PyObject *args, PyObject *kwds)
             }
             Py_INCREF(dtypes[0]);
             a = (PyArrayObject *)PyArray_FromArray(operands[0], dtypes[0],
-                                        NPY_ARRAY_ALIGNED|NPY_ARRAY_UPDATEIFCOPY);
+                                        NPY_ARRAY_ALIGNED);
             if (a == NULL) {
                 goto fail;
             }
