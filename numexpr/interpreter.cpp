8203febae49ad25d427723c3c1ace09716188e37
Thomas A Caswell <tcaswell@gmail.com>
2022-04-20 19:27:42 -0400

    FIX: do not use deprecated numpy flag
    Per
    https://numpy.org/doc/stable/reference/c-api/array.html#c.NPY_ARRAY_WRITEBACKIFCOPY
    and
    https://numpy.org/doc/stable/reference/c-api/array.html#c.NPY_ARRAY_UPDATEIFCOPY
    NPY_ARRAY_WRITEBACKIFCOPY is preferred.

--- numexpr/interpreter.cpp
+++ numexpr/interpreter.cpp
@@ -1271,7 +1271,7 @@ NumExpr_run(NumExprObject *self, PyObject *args, PyObject *kwds)
             // a = (PyArrayObject *)PyArray_FromArray(operands[0], dtypes[0],
             //                             NPY_ARRAY_ALIGNED|NPY_ARRAY_WRITEBACKIFCOPY);
             a = (PyArrayObject *)PyArray_FromArray(operands[0], dtypes[0],
-                                        NPY_ARRAY_ALIGNED|NPY_ARRAY_UPDATEIFCOPY);
+                                        NPY_ARRAY_ALIGNED|NPY_ARRAY_WRITEBACKIFCOPY);
             if (a == NULL) {
                 goto fail;
             }
