295f26b46059f72c7f9361a28e03537da66f9bd9
Francesc Alted <francesc@blosc.org>
2024-06-20 19:57:49 +0200

    Add a README in pyproject.toml also

--- setup.py
+++ setup.py
@@ -31,9 +31,8 @@ with open('numexpr/version.py', 'w') as fh:
         pass
     fh.write("platform_machine = '%s'\n" % platform.machine())
 
-# Read the contents of the README file
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+# Read the contents of your README file
+with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 lib_dirs = []
